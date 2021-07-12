3871013a5c81c6a1a9246f16f1c33578c8ca8901
Kevin Kurt <kekevekevin@gmail.com>
2021-07-12 00:50:37 -0300

    commit

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
+
+    return this.http.post<UserLogin>('https://pessoalbackkkmdss.herokuapp.com/usuarios/logar', userLogin)
+
+  }
+
+  cadastrar(user: User): Observable<User>{
+    return this.http.post<User>('https://pessoalbackkkmdss.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  geByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://pessoalbackkkmdss.herokuapp.com/usuarios/${id}`)
+  }
+
+  logado(){
+    let ok = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
+
+  }
+
+
+}
